fd3ab92d93ddf3b463aa1d3ee187fbadc1cc34a6
BAMHiggin <bamhiggin@gmail.com>
2019-03-22 23:44:16 -0500

    created category model

---
+++ models/category.js
@@ -0,0 +1,25 @@
+const categorySeeds = require("./scripts/categorySeeds");
+
+module.exports = function (sequelize, DataTypes) {
+  //Define the category table
+  let Category = sequelize.define("Category", {
+    name: DataTypes.STRING
+  });
+
+  Category.associate = function (models) {
+    // Associating category with Projects
+    Category.hasMany(models.Project, {
+        onDelete: "cascade"
+    });
+  };
+
+  //Insert category seed data
+  Category.sync().then(() => {
+    Category.bulkCreate(categorySeeds, {
+      ignoreDuplicates: true
+    });
+  });
+
+  return Category;
+};
+
