1b86326ef8c92dc88b605494e791e018170e4b87
BAMHiggin <bamhiggin@gmail.com>
2019-03-29 22:52:48 -0500

    carousel added, awaiting card styling

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -11,6 +11,9 @@ import { Col, Row, Container } from '../components/Grid';
 import { List } from '../components/List';
 import Project from '../components/Project';
 import Tiles from '../components/Tiles';
+import SplashCarousel from '../components/Carousel';
+
+
 
 class Splash extends Component {
 
@@ -25,21 +28,21 @@ class Splash extends Component {
 
     getCarouselProjects = () => {
         API.getCarouselProjects()
-            .then(res => 
+            .then(res =>
                 // console.log(res.data.date)
-                
+
                 this.setState({
                     projects: res.data
                 })
-                
+
             )
-            .catch(() => 
+            .catch(() =>
                 this.setState({
                     projects: [],
                     message: 'Uh oh our search is going wrong'
                 })
             );
-            
+
     };
 
     render() {
@@ -52,35 +55,43 @@ class Splash extends Component {
                                 <strong>THIS IS THE TEST DISPLAY SECTION</strong>
                             </h1>
                             <h2 className='text-center'>
-                            Fingers Crossed it works?!?!?!?!
+                                Fingers Crossed it works?!?!?!?!
                             </h2>
-                            <button className='btn btn-primary color-prim' style={{marginRight: 25, position: 250}}>Find Ways to Help</button>
+                            <button className='btn btn-primary color-prim' style={{ marginRight: 25, position: 250 }}>Find Ways to Help</button>
                             <button className='btn btn-primary color-prim'>Create a Project</button>
                         </Jumbotron>
                     </Col>
                 </Row>
+                {/* testing space */}
+                {/* <Row>
+                    <Col size='md-12'>
+                        <SplashCarousel>
+
+                        </SplashCarousel>
+                    </Col>
+                </Row> */}
                 <Row>
                     <Col size='md-12'>
                         <Tiles title='This is only a test'>
                             {this.state.projects.length ? (
-                                <List>
-                                    {this.state.projects.map(project => (
-                                        <Project
-                                            key={project.id}
-                                            title={project.title}
-                                            description ={project.description}
-                                            total_hours ={project.total_hours}
-                                            date = {project.data}
-                                            start_time= {project.start_time}
-                                            end_time = {project.end_time}
-                                            location = {project.location}
-                                        />
-                                    ))}
-                                </List>
+                                <SplashCarousel>
+                                        {this.state.projects.map(project => (
+                                            <Project
+                                                key={project.id}
+                                                title={project.title}
+                                                description={project.description}
+                                                total_hours={project.total_hours}
+                                                date={project.data}
+                                                start_time={project.start_time}
+                                                end_time={project.end_time}
+                                                location={project.location}
+                                            />
+                                        ))}
+                                </SplashCarousel>
                             ) : (
-                                <h2 className='text-center'>{this.state.message}</h2>
-                            )
-                        }
+                                    <h2 className='text-center'>{this.state.message}</h2>
+                                )
+                            }
                         </Tiles>
                     </Col>
                 </Row>
