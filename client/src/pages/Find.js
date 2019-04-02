a6d50aa2f8968a2b48f859587ab551d27d27dc06
Paul <paul@cwik.us>
2019-04-02 12:30:23 -0500

    Added category filter to the find page

--- client/src/pages/Find.js
+++ client/src/pages/Find.js
@@ -71,11 +71,31 @@ class Find extends Component {
 
         <Container>
           <Row>
+            <div className="dropdown show">
+              <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Category Filter</button>
+
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a className="dropdown-item" href="/find/all">None</a>
+                <a className="dropdown-item" href="/find/category/1">Environmental</a>
+                <a className="dropdown-item" href="/find/category/2">Hunger and Food Security</a>
+                <a className="dropdown-item" href="/find/category/3">Education and Literacy</a>
+                <a className="dropdown-item" href="/find/category/4">Crisis Support and Disaster Relief</a>
+                <a className="dropdown-item" href="/find/category/5">Elder Care</a>
+                <a className="dropdown-item" href="/find/category/6">Vulnerable Groups</a>
+                <a className="dropdown-item" href="/find/category/7">Animal Welfare</a>
+                <a className="dropdown-item" href="/find/category/8">Housing and Shelter</a>
+                <a className="dropdown-item" href="/find/category/9">Community Projects</a>
+                <a className="dropdown-item" href="/find/category/10">Children and Youth</a>
+              </div>
+            </div>
+          </Row>
+          <Row>
             {this.state.projects.map(project => (
               <Col size='md-4'>
                 <TestTile
                   id={project.id}
-                  
+
                   title={project.title}
                   date={project.date}
                   photo_url={project.photo_url}
