aaf52d934badb0745ab14fee97b0286cde91e8f0
ZGermain <zack.germain@gmail.com>
2019-04-01 22:01:50 -0500

    'updated find page'

--- client/src/components/TestTile/index.js
+++ client/src/components/TestTile/index.js
@@ -4,9 +4,11 @@ import Moment from 'react-moment';
 import { Row, Col } from "../Grid";
 import "./style.css";
 
-function TestTile({ title, category, date, photo_url }) {
+function TestTile({ id, title, category, date, photo_url }) {
     return (
         <div className='tile-full'>
+            <a href={'/find/projects/' + id}>
+
             <Row className="flex-wrap-reverse">
                 <Col size='md-12'>
                     <img className='img-fluid tile-image' src={photo_url} alt={title}></img>
@@ -19,6 +21,8 @@ function TestTile({ title, category, date, photo_url }) {
                     <div className='tile-category'>{category}</div>
                 </Col>
             </Row>
+                            
+            </a>
         </div>
     );
 }
