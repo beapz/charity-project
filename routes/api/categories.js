9fae207b812dad2874055067b8a52ee864073928
Brittany <brittany.higgin@northwestern.edu>
2019-04-03 10:19:46 -0500

    Revert "commented out bootstrap 3 link, dropdown now bootstrap 4"

--- routes/api/categories.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const catController = require("../../controllers/catController");
-const db = require("../../models");
-
-//This route is /API/categories
-router.route('/')
-    .get(catController.findAll);
