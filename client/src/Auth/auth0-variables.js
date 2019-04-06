b395b71b072fa77d348ebe907b3bd8efcd2cff38
Amena Ahmed <am.ahmed09@gmail.com>
2019-04-06 08:51:27 -0500

    changed call back to revert to find all page from create. added css to jumbotron.

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -2,8 +2,8 @@ export const AUTH_CONFIG = {
     domain: 'dev-ihaxg122.auth0.com',
     clientId: 'BGuUTUSNZqAVI4357b4r0QQzgfUhtS2h',
     clientSecret: 'zDon8n2To_l_po3SxdAYWiHhDaKdnTM93E88K-ZT305Coly_0E2tMVmjKI963DtO',
-    callbackUrl: 'https://timelender.herokuapp.com/callback'
-    // callbackUrl: 'http://localhost:3000/callback'
+    // callbackUrl: 'https://timelender.herokuapp.com/callback'
+    callbackUrl: 'http://localhost:3000/callback'
 
   }
 
